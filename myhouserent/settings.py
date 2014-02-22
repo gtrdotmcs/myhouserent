f69eaa7ff7967e4a1ce3fbc70b27c8d32b21f343
gtrdotmcs <gtr.mcs@gmail.com>
2014-02-22 03:55:52 +0000

    venev is now on git hub

--- myhouserent/settings.py
+++ myhouserent/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+# heroku requirement
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/action/workspace/myhouserent/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
