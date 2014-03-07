e77e7ab557100f6af220d7ecb282b0dc27d3e805
ghanshyam <gtr.mcs@gmail.com>
2014-03-07 10:52:26 +0530

    Delete urls.py
    adding it

--- houseowner/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from houseowner import views
-# maps urls
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<houseowner_id>\d+)/$', views.detail, name='detail')
-    
-)
