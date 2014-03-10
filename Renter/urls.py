436e10e2699e8aa3b5bb9b18f9e6e92d51b88ac5
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-10 11:49:10 +0530

    added renter templets

---
+++ Renter/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from Renter import views
+
+# maps urls
+urlpatterns = patterns('',
+    url(r'^(?P<houseowner_id>\d+)$', views.editdetails, name='editdetails'),
+    #url(r'^HOspcf/(?P<houseowner_id>\d+)/$', views.detail, name='detail')
+    
+)
