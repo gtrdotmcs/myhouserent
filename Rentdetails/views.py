a9a7886345639365b430f6b96932193b8732f8e5
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-10 18:11:08 +0530

    here foreginkey will take care and context is also sent to it

--- Rentdetails/views.py
+++ Rentdetails/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from Renter.models import RenterInfo
 from Rentdetails.models import RentDetails
     
 def editrentdetails(request, rentdetail_id):
-    
+    renter = RenterInfo.objects.get(id=rentdetail_id)
+    rentername = renter.full_name
     rentdetails = get_object_or_404(RentDetails, pk=rentdetail_id)
-    return render(request, 'rentdetails/Rentdetail.html', {'rentdetails': rentdetails})
+    context = {'rentdetails': rentdetails, 'rentername': rentername}
+    return render(request, 'rentdetails/Rentdetail.html', context )
