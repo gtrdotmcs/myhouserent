cbd79ca9fa44992bda70b415815627a5a8dd657f
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-13 16:13:20 +0530

    getting data on post need to save now to db

--- Rentdetails/urls.py
+++ Rentdetails/urls.py
@@ -4,7 +4,9 @@ from Rentdetails import views
 
 # maps urls
 urlpatterns = patterns('',
-    url(r'^(?P<rentdetail_id>\d+)$', views.inforentdetails, name='inforentdetails'),
+    url(r'^(?P<rentdetail_id>\d+)/info$', views.inforentdetails, name='inforentdetails'),
+    url(r'^(?P<rentdetail_id>\d+)/edit$', views.editrentdetails, name='editrentdetails'),
+    url(r'^(?P<rentdetail_id>\d+)/submit$', views.submitrentdetails, name='submitrentdetails'),
     #url(r'^HOspcf/(?P<houseowner_id>\d+)/$', views.detail, name='detail')
     
 )
--- Rentdetails/views.py
+++ Rentdetails/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from Rentdetails.models import RentDetails
     
 def inforentdetails(request, rentdetail_id):
     rentdetails = get_object_or_404(RentDetails, pk=rentdetail_id)
     return render(request, 'rentdetails/Rentdetail.html', {'rentdetails': rentdetails} )
+
+def editrentdetails(request, rentdetail_id):
+    rentdetails = get_object_or_404(RentDetails, pk=rentdetail_id)
+    return render(request, 'rentdetails/editrentdetails.html', {'rentdetails': rentdetails} )
+
+def submitrentdetails(request, rentdetail_id):
+    
+    try:
+        rentdetails = get_object_or_404(RentDetails, pk=rentdetail_id)
+        
+    except (KeyError, RentDetails.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'rentdetails/editrentdetails.html', {
+            'rentdetails': rentdetails_id,
+            'error_message': "You didn't correctly edited.",
+        })
+    else:
+        print request.POST['months']
+        print request.POST['Dategiven']
+        print request.POST['pyeeadv']
+        print request.POST['Amount']
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('Rentdetails:inforentdetails', args=(u'1',)))
