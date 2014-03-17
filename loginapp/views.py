aa3f03079965115af20f8b6338c95f337629aae0
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-17 18:06:34 +0530

    login externaly by user

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
 
@@ -15,10 +16,10 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                state = "You're successfully logged in!"
+                state = "You're successfully logged in! %s"%username
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
 
-    return render_to_response('logged_in.html',{'state':state, 'username': username})
+    return render_to_response('logged_in.html',{'state':state, 'username': username}, context_instance=RequestContext(request))
