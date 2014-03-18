f128e2d49482240a996d60f06bf7406b7055c897
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-18 19:29:04 +0530

    adde some more changes as to test in html page for user autho and display msg

--- houseowner/views.py
+++ houseowner/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from houseowner.models import HouseOwner 
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/user/login/')
+#@login_required(login_url='/user/login/')
 def index(request):
     #latest_houseowner_list = HouseOwner.objects.order_by()
     #output = ', '.join([p.Full_Name for p in latest_houseowner_list])
@@ -14,7 +14,7 @@ def index(request):
     context = {'Houseowner_list': Houseowner_list}
     return render(request, 'houseowner/houseownerdetails.html', context)
 
-@login_required(login_url='/user/login/')
+#@login_required(login_url='/user/login/')
 def detail(request, houseowner_id):
     houseowner = get_object_or_404(HouseOwner, pk=houseowner_id)
     return render(request, 'houseowner/details.html', {'houseowner': houseowner})
--- loginapp/views.py
+++ loginapp/views.py
@@ -6,7 +6,6 @@ from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
 
 def login_user(request):
-    state = "Please log in below..."
     username = password = ''
     if request.POST:
         username = request.POST.get('username')
@@ -16,17 +15,17 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                state = "You're successfully logged in! %s"%username
-            else:
-                state = "Your account is not active, please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
+#                 state = "You're successfully logged in! %s"%username
+#             else:
+#                 state = "Your account is not active, please contact the site admin."
+#         else:
+#             state = "Your username and/or password were incorrect."
 
-    return render(request, 'logged_in.html', {'state':state, 'username': username})
+    return render(request, 'logged_in.html', {'username': username})
 
 def logout_view(request):
     logout(request)
-    state = "logout success if you want you can log in below..."
+#    state = "logout success if you want you can log in below..."
     username = password = ''
-    return render(request, 'logged_in.html',{'state':state, 'username': username})
+    return render(request, 'logged_in.html',{'username': username})
     # Redirect to a success page.
