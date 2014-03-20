1c61fb12d16f6c15972df6f25a92f824d67561a2
gtrdotmcs <gtr.mcs@gmail.com>
2014-03-20 19:03:11 +0530

    design started yo! now login page have been desgin an static folder added for js and css files

--- houseowner/views.py
+++ houseowner/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from houseowner.models import HouseOwner 
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
-#@login_required(login_url='/user/login/')
+@login_required(login_url='/user/login/')
 def index(request):
     #latest_houseowner_list = HouseOwner.objects.order_by()
     #output = ', '.join([p.Full_Name for p in latest_houseowner_list])
