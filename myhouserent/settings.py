64d4688b2129b59791c9c1424b47fb4548f3c8dd
gtrdotmcs <gtr.mcs@gmail.com>
2014-10-20 18:24:53 +0530

    upgrade the django version 1.6.2 to 1.6.6 and some setting chnage runs for foremen

--- myhouserent/settings.py
+++ myhouserent/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    STATIC_ROOT,
+    os.path.join(BASE_DIR, os.path.join("myhouserent","static")),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
